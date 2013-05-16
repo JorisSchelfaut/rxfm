3b9f9c94b9cc0c802b0861226a04496f4dcdc21f
JorisSchelfaut <joris.schelfaut@hotmail.com>
2013-05-16 23:23:50 +0200

    rx.fm 1.0

--- js/init.js
+++ js/init.js
@@ -1,59 +1,46 @@
 var DEBUG = false;
-/*
-if (Modernizr.localstorage) {
-    // good times
-    console.log('Good times.');
-} else {
-    // bad times
-    console.log('Bad times.');
-}
-*/
-var KEY = 'user';
-var ACTIVE_USER = localStorage.getItem(KEY);
-if (ACTIVE_USER) {
-    getSessionKeyForUser(ACTIVE_USER);
-} else {
-    ACTIVE_USER = window.prompt('Type a username : ', 'username');
-    localStorage.setItem(KEY, ACTIVE_USER);
-    getSessionKeyForUser(ACTIVE_USER);
-}
 
-getSessionKeyForUser = function (user) {
-    
-    var KEY_SESSION = 'session-' + user;
-    var SESSION_KEY = localStorage.getItem(KEY_SESSION);
-    
-    if (SESSION_KEY) {
-        // We're done.
-    } else {
-        // Get a session key!
-        var API_KEY         = 'a7eec810bcefeb721b140a929b474983';
-        var API_SECRET      = 'ab5950262d6fb5069bc14d718d13e5f7';
-        var LAST_FM = new LastFM({
-            apiKey    : API_KEY,
-            apiSecret : API_SECRET,
-            cache     : new LastFMCache()
-        });
-        var authURL = 'http://www.last.fm/api/auth/?api_key='
-            + API_KEY + '&cb=' + window.location;
-        var token = '';
-        if (!$.url().param('token')) {
-            window.location = authURL;
-        } else {
-            token = $.url().param('token');
+// Verifying if there are stored settings.
+var hasStoredSettings = localStorage.getItem('user');
+if (DEBUG) console.log('ACTIVE_USER == ' + ACTIVE_USER);
 
-            LAST_FM.auth.getSession({
-                token: token
-            }, {
-                success: function(data) {
-                    SESSION_KEY = data.session.key;
-                    localStorage.setItem(KEY_SESSION, SESSION_KEY);
-                },
-                error : function (data) {}
-            });
-        }
-        
+if (hasStoredSettings) {
+    // We're done.
+    if (DEBUG) {
+        var ACTIVE_USER = localStorage.getItem(KEY);
+        var SESSION_KEY = localStorage.getItem('session-' + ACTIVE_USER);
+        console.log('ACTIVE_USER == ' + ACTIVE_USER);
+        console.log('SESSION_KEY == ' + SESSION_KEY);
     }
-    
-    
-};
+} else {
+    // Get a session key!
+    var API_KEY = 'a7eec810bcefeb721b140a929b474983';
+    var API_SECRET = 'ab5950262d6fb5069bc14d718d13e5f7';
+    var LAST_FM = new LastFM({
+        apiKey    : API_KEY,
+        apiSecret : API_SECRET,
+        cache     : new LastFMCache()
+    });
+    var authURL = 'http://www.last.fm/api/auth/?api_key=' + API_KEY + '&cb=' + window.location.href;
+    if (!$.url().param('token')) {
+        window.location.href = authURL;
+    } else {
+        LAST_FM.auth.getSession({
+            token: $.url().param('token')
+        }, {
+            success: function(data) {
+                SESSION_KEY = data.session.key;
+                ACTIVE_USER = data.session.name;
+                if (DEBUG) {
+                    console.log('ACTIVE_USER == ' + ACTIVE_USER);
+                    console.log('SESSION_KEY == ' + SESSION_KEY);
+                }
+                localStorage.setItem('user', ACTIVE_USER);
+                localStorage.setItem('session-' + ACTIVE_USER, SESSION_KEY);
+            },
+            error : function (data) {
+                console.error('Error occurred when getting a Last.fm session.');
+            }
+        });
+    }
+}
--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,15 @@
 var KEY = 'user';
 var ACTIVE_USER = localStorage.getItem(KEY);
 var SESSION_KEY = localStorage.getItem('session-' + ACTIVE_USER);
+
+reset = function () {
+    
+    // Remove session;
+    localStorage.removeItem('session-' + ACTIVE_USER);
+    
+    // Remove user;
+    localStorage.removeItem(KEY);
+    
+    var root = window.location.toString().split('settings.html')[0];
+    window.location.href = root;
+};
